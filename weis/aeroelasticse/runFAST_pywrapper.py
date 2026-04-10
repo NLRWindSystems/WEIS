ddc1544b0a974155ae47a569846ee9ce626ca9f5
ptrbortolotti <ptrbortolotti@gmail.com>
2026-04-10 12:07:00 -0600

    update iea15 input deck

--- weis/aeroelasticse/runFAST_pywrapper.py
+++ weis/aeroelasticse/runFAST_pywrapper.py
@@ -18,7 +18,7 @@ from pyOpenFAST.fast import FastLibAPI
 import numpy as np
 
 # Realpath will resolve symlinks
-of_path = "/Users/pbortolo/miniconda3/envs/weis-env/bin/openfast"
+of_path = os.path.realpath( shutil.which('openfast') )
 bin_dir  = os.path.dirname(of_path)
 lib_dir  = os.path.abspath( os.path.join(os.path.dirname(bin_dir), 'lib') )
 
